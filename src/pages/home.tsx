b0aa0cab09630101d7feaae6b386ae96cc1d1c54
mark <mark77234@naver.com>
2025-08-02 00:25:19 +0900

    fix: 안쓴느 컴포넌트제거

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,5 @@
 import StatusBar from "../components/home/Statusbar";
 
-function IphoneStatusBar() {
-  return (
-    <div className="iphone-status-bar">
-      <span className="iphone-status-time">9:41</span>
-      <span className="iphone-status-icons">
-        <span className="iphone-status-signal" />
-        <span className="iphone-status-wifi" />
-        <span className="iphone-status-battery" />
-      </span>
-    </div>
-  );
-}
-
 export default function Home() {
   return (
     <div className="main-background">
