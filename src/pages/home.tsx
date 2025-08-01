327b9e1b7fcdc965d4c3f229bbb652c6eb6cbc28
mark <mark77234@naver.com>
2025-08-01 22:37:13 +0900

    feat: 초기설정

---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import MainSreen from "../components/home/MainScreen";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="iphone-container">
+        <MainSreen />
+
+        <div className="iphone-home-indicator" />
+      </div>
+    </div>
+  );
+}
